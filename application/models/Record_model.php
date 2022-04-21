afe58e099c9f3f30e9ad41f422534c7f5a0b899d
Ferry Kurniawan <92605921+ferryu46@users.noreply.github.com>
2022-04-22 03:52:29 +0700

    Update Record_model.php

--- application/models/Record_model.php
+++ application/models/Record_model.php
@@ -8,7 +8,7 @@ class Record_model extends CI_Model {
     public function __construct() {
         $this->client = new Client([
             // TODO: Tambahkan Base URL API
-            'base_uri' => "http://34.101.222.137/var/www/html/front-end-money_tracker/",
+            'base_uri' => "http://34.101.222.137/front-end-money_tracker/",
         ]);
     }
 
